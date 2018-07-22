f8c1ccf802de534236afa7c7b53eaac5693b5a08
boyonger <32034024+boyonger@users.noreply.github.com>
2018-07-23 00:35:45 +0800

    youdaoDemo

---
+++ yodaoDemo/src/main/java/hibernateTest.java
@@ -0,0 +1,12 @@
+import com.youdaoDemo.entity.User;
+import lombok.extern.java.Log;
+
+import java.io.IOException;
+
+@Log
+public class hibernateTest {
+    public static void main(String[] args) throws IOException {
+        User user=new User();
+        
+    }
+}
